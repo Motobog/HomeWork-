e7dea333b04ed012b8694d63bec1af1808cf9fac
Bogdan <4bogdan1977@gmail.com>
2022-06-08 17:17:59 +0300

    Task2 added

--- HomeWork_1/Program.cs
+++ HomeWork_1/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Task1
-double num1,num2;
+/*double num1,num2;
 Console.Write("Input first number: ");
 num1=Convert.ToDouble(Console.ReadLine());
 Console.Write("Input second number: ");
@@ -11,4 +11,16 @@ if (num1>=num2)
 else
 {
    Console.WriteLine("Max is: " + num2); 
-}
+}*/
+//Task2
+double num1, num2, num3, max;
+Console.Write("Input first number: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input second number: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input third number: ");
+num3 = Convert.ToDouble(Console.ReadLine());
+max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine("Max is: " + max);
