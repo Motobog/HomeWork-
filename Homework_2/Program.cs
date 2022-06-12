6a943b83163f28ec85f5fdd85cebfbfd0275718d
Bogdan <4bogdan1977@gmail.com>
2022-06-12 23:06:14 +0300

    Task1 added

---
+++ Homework_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Task1
+void IntSec()
+{
+    Console.Write("Input three digit number: ");
+    int num=Convert.ToInt32(Console.ReadLine());
+    if ((num<1000 && num>99) || (num<-99 && num>-1000))
+    {
+        int res=Math.Abs(num)/10%10;
+        Console.WriteLine($"Two digital is {res}");
+    }
+    else
+    {
+        Console.WriteLine("Input Correct number!");
+    }
+}
+IntSec();
