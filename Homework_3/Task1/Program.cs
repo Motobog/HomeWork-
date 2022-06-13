77a2273a51cb2b436e688e388e16b31814219f8e
Bogdan <4bogdan1977@gmail.com>
2022-06-13 23:07:40 +0300

    Task3_1 Add

---
+++ Homework_3/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿string PalCheck(int num)
+{
+    int[] digit = new int[5];
+    for (int i = 0; i <= 4; i++)
+    {
+        digit[i] = num % 10;
+        num /= 10;
+    }
+    if (digit[0] == digit[4] && digit[1] == digit[3]) return "Polindrom";
+    else return "Not Polindrom";
+}
+
+Console.Write("Input 5 digigtal number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ( number > 99999 || number < -99999 || number > -10000 && number < 10000) Console.WriteLine("Input correct number!");
+else Console.WriteLine(PalCheck(number));
