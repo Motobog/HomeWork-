127b7df64b4e40534b02a9586c9f20d0cdb3c0dc
Bogdan <4bogdan1977@gmail.com>
2022-06-25 17:26:07 +0300

    Task6_1

---
+++ Homework_6/Task6_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 0, 7, 8, -2, -2 -> 2
+int MaxValue(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"Enter {i} nature number: ");
+        bool succeses = int.TryParse(Console.ReadLine(), out int result);
+        if (succeses)
+        {
+            if (succeses && result > 0) count++;
+        }
+        else
+        {
+            i--;
+            Console.WriteLine("Enter a valid value!");
+        }
+    }
+    return count;
+}
+int res = 0;
+Console.Write("Enter the number of integers to enter (number must be natural): ");
+bool success = int.TryParse(Console.ReadLine(), out int number);
+if (success && number > 0) res = MaxValue(number);
+else Console.Write($"Enter correct number!");
+Console.WriteLine($"Number of positive numbers is {res}");
