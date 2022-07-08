5689c17630deece579cb9f7ed5848644cde9f4f3
Bogdan <4bogdan1977@gmail.com>
2022-07-09 00:41:00 +0300

    Task9_1

---
+++ Homework_9/Task9_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+int SumNaturalNumbers(int min, int max)
+{
+    if(max>min) return max + SumNaturalNumbers(min,max-1);
+    else return min;
+
+}
+
+Console.Clear();
+
+Console.Write("enter the first natural number of the range: ");
+bool sucsessStartInterval = int.TryParse(Console.ReadLine(), out int startInterval);
+Console.Write("Enter the second natural number greater than the first: ");
+bool sucsessEndInerval = int.TryParse(Console.ReadLine(), out int endInterval);
+if (sucsessStartInterval && sucsessEndInerval && startInterval > 0 && endInterval > startInterval)
+    Console.WriteLine(SumNaturalNumbers(startInterval, endInterval));
+else Console.WriteLine("Input correct numbers");
